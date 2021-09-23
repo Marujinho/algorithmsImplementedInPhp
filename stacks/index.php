3f331be37ef6aa7d5f7906a8a4db310244d76e6a
Marujinho <aka.contato@gmail.com>
2021-09-23 00:42:33 -0300

    stacks complete

---
+++ stacks/index.php
@@ -0,0 +1,44 @@
+<?php
+
+require_once('LinkedList.php');
+
+class Stack{
+
+    private $head;
+
+    public function getHead()
+    {
+        return $this->head;   
+    }
+
+    public function setHead($head)
+    {
+        $this->head = $head;   
+    }
+
+    public function push($data)
+    {
+        $newNode = new Node($data);
+        $newNode->setNext($this->head);
+
+        $this->head = $newNode; 
+    }
+
+    public function pop()
+    {
+        if($this->head == null) {
+            throw new Exception("Stack is Empty", 1);
+        }
+
+        $this->head = $this->head->getNext();   
+    }   
+}
+
+$stack = new Stack();
+$stack->push('AA');
+$stack->push('BB');
+$stack->push('CC');
+$stack->pop();
+print_r( $stack->getHead() );
+
+
